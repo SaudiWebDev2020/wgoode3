617b9990b31d05150b7b0c028a99305052db32ba
William Goode <wgoode3@gmail.com>
2020-11-23 07:10:11 -0500

    start ccg

---
+++ week7/ccg.js
@@ -0,0 +1,21 @@
+class Card {
+
+}
+
+class Unit extends Card {
+
+    // attacks another unit
+    attack(otherUnit) {
+
+    }
+
+}
+
+class Effect extends Card {
+
+    // modifies a unit
+    play(someUnit) {
+
+    }
+
+}
