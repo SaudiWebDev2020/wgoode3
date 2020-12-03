f0f35962727834a313c60298196e1727d94068f0
William Goode <wgoode3@gmail.com>
2020-12-03 02:15:49 -0500

    algo

---
+++ week8/w8d5.js
@@ -0,0 +1,24 @@
+function allCombinations(words) {
+    const combinations = [];
+    // your code here
+    function helper(sub, i) {
+        if(i >= words.length) {
+            combinations.push(sub.substring(1));
+        } else {
+            for(let word of words[i]) {
+                helper(sub + "  " + word, i+1);
+            }
+        }
+    }
+    helper("", 0);
+    return combinations;
+  }
+  
+  const words = [ ["quick", "lazy"],
+                  ["fluffy", "scraggly"],
+                  ["brown", "red", "grey"],
+                  ["fox", "dog"],
+                  ["!", "?"] ];
+  
+  console.log( allCombinations( words ) );
+  // should return ["quick brown fox", "quick brown dog", "quick red fox", "quick red dog", "quick grey fox", "quick grey dog", "lazy brown fox", "lazy brown dog", "lazy red fox", "lazy red dog", "lazy grey fox", "lazy grey dog"]
