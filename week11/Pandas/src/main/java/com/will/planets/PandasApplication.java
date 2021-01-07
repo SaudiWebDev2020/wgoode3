abf724b3ccfd1002f5bbb035857fe8b20a177d24
William Goode <wgoode3@gmail.com>
2021-01-07 07:00:59 -0500

    added pandas

---
+++ week11/Pandas/src/main/java/com/will/planets/PandasApplication.java
@@ -0,0 +1,13 @@
+package com.will.planets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandasApplication.class, args);
+	}
+
+}
---
+++ week11/Pandas/src/main/resources/static/js/main.js
@@ -0,0 +1,3 @@
+// alert("hello");
+
+console.table([["hello", "world"], ["how", "are you?"]]);
