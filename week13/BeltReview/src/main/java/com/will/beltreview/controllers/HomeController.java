7b3aa8e1b4a00a2831a8a3292fcc5401b13d70e2
William Goode <wgoode3@gmail.com>
2021-01-17 07:08:59 -0500

    added login and registration

---
+++ week13/BeltReview/src/main/java/com/will/beltreview/controllers/HomeController.java
@@ -0,0 +1,63 @@
+package com.will.beltreview.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.will.beltreview.models.LoginUser;
+import com.will.beltreview.models.User;
+import com.will.beltreview.services.UserService;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private UserService userServ;
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("newUser", new User());
+		model.addAttribute("newLogin", new LoginUser());
+		return "index.jsp";
+	}
+	
+	@PostMapping("/register")
+	public String register(@Valid @ModelAttribute("newUser") User newUser, 
+			BindingResult result, Model model, HttpSession session) {
+		userServ.register(newUser, result);
+		if(result.hasErrors()) {
+			model.addAttribute("newLogin", new LoginUser());
+			return "index.jsp";
+		} else {
+			session.setAttribute("user_id", newUser.getId());
+			return "redirect:/";
+		}
+	}
+	
+	@PostMapping("/login")
+	public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, 
+			BindingResult result, Model model, HttpSession session) {
+		User u = userServ.login(newLogin, result);
+		if(result.hasErrors()) {
+			model.addAttribute("newUser", new User());
+			return "index.jsp";
+		}
+		session.setAttribute("user_id", u.getId());
+		return "redirect:/";
+		
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("user_id");
+		return "redirect:/";
+	}
+	
+}
---
+++ week13/BeltReview/src/main/java/com/will/beltreview/services/UserService.java
@@ -0,0 +1,65 @@
+package com.will.beltreview.services;
+
+import java.util.Optional;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import com.will.beltreview.models.LoginUser;
+import com.will.beltreview.models.User;
+import com.will.beltreview.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	public User findOne(Long id) {
+		Optional<User> potentialUser = userRepo.findById(id);
+		return potentialUser.isPresent() ? potentialUser.get() : null;
+	}
+	
+	public User findOne(String email) {
+		Optional<User> potentialUser = userRepo.findByEmail(email);
+		return potentialUser.isPresent() ? potentialUser.get() : null;
+	}
+	
+	public User register(User newUser, BindingResult result) {
+		if(findOne(newUser.getEmail()) != null) {
+			result.rejectValue("email", "Unique", "This email is already in use!");
+		}
+		if(!newUser.getPassword().equals(newUser.getConfirm())) {
+			result.rejectValue("confirm", "Matches", "The Confirm Password must match Password!");
+		}
+		if(result.hasErrors()) {
+			return null;
+		} else {
+			String hashed = BCrypt.hashpw(newUser.getPassword(), BCrypt.gensalt());
+			newUser.setPassword(hashed);
+			return userRepo.save(newUser);
+		}
+	}
+	
+	public User login(LoginUser newLogin, BindingResult result) {
+		if(result.hasErrors()) {
+			return null;
+		}
+		User user = findOne(newLogin.getEmail());
+		if(user == null) {
+			result.rejectValue("email", "Unique", "Unknown email!");
+			return null;
+		}
+		if(!BCrypt.checkpw(newLogin.getPassword(), user.getPassword())) {
+			result.rejectValue("password", "Matches", "Invalid Password!");
+		}
+		if(result.hasErrors()) {
+			return null;
+		} else {
+			return user;
+		}
+	}
+	
+}
